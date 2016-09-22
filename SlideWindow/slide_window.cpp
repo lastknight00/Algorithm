346e6049b38b50360e1b2705bd7bb7ef688e087b
lastknight00 <darkknight86@hanmail.net>
2016-09-22 21:06:17 +0900

    슬라이딩 윈도우 변경

--- SlideWindow/slide_window.cpp
+++ SlideWindow/slide_window.cpp
@@ -1,51 +1,16 @@
-#include <cstdio>
-#include<string.h>
-int left[2000], right[2000], cache[2000][2000];
-int N, Answer = 0;;
- 
-int rec(int, int);
- 
-int main() {
-    scanf("%d", &N);
-    for (int index = 0; index < N; index++) {
-        scanf("%d", &left[index]);
-    }
-    for (int index = 0; index < N; index++) {
-        scanf("%d", &right[index]);
-    }
-    memset(cache,-1,sizeof(cache));
-    Answer = rec(N - 1, N - 1);
-    printf("%d", Answer);
-}
- 
-int rec(int lpoint, int rpoint) {
-    int retval = 0;
-    if (lpoint < 0 || rpoint < 0) {
-        return 0;
-    }
-    else if (cache[lpoint][rpoint] >= 0) {
-        return cache[lpoint][rpoint];
-    }
-    else
-    {
-        if (left[lpoint] > right[rpoint]) {
-            retval += right[rpoint];
-            retval += rec(lpoint, rpoint - 1);
-        }
-        else {
-            int temp = rec(lpoint - 1, rpoint);
-            int temp1 = rec(lpoint - 1, rpoint - 1);
- 
-            if (temp >= temp1) {
-                retval += temp;
-            }
-            else {
-                retval += temp1;
-            }
-        }
-    }
-     
-    cache[lpoint][rpoint] = retval;
- 
-    return retval;
+#include<cstdio>
+#include<vector>
+#include<queue>
+using namespace std;
+int n,m,d,i;
+deque<pair<int,int> > dq;
+int main(){
+	scanf("%d %d",&n,&m);
+	for(i=0;i<n;i++){
+		scanf("%d",&d);
+		while(!dq.empty()&&dq.front().second<=i-m)dq.pop_front();
+		while(!dq.empty()&&dq.back().first>d)dq.pop_back();
+		dq.push_back(make_pair(d,i));
+		printf("%d ",dq.front());
+	}
 }
